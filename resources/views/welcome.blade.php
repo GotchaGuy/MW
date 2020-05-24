dfd244ccb718154634e756c5940f88c66cb7a465
GotchaGuy <aljosa100@gmail.com>
2020-05-25 00:45:37 +0200

    set up db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Aj Doniraj
                 </div>
 
                 <div class="links">
